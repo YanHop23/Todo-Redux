bf0ab51dffcd37f7e582150765a02ae708b9ec0b
Polothevets Oleg <olegpolothevets@gmail.com>
2023-11-09 12:08:40 +0200

    add components

--- todoredux/src/components/App.js
+++ todoredux/src/components/App.js
@@ -1,12 +1,14 @@
+import React from 'react';
+import Footer from './Footer';
+import AddTodo from '../containers/AddTodo';
+import VisibleTodoList from '../containers/VisibleTodoList';
 
-import './../App.css';
-
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+);
 
 export default App;
